2ec689aa127796986783b7861df4569e266af9ce
Phillip Bement <phillipb@pbement.com>
2023-04-25 23:45:33 -0700

    fix project url

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ with open("README.md", "r") as f:
 
 setup(
   name="empirical_init",
-  version="0.0.1",
+  version="0.0.2",
   description="Automatically initialize weights in pytorch modules using a hacky empirical method.",
   long_description=long_description,
   long_description_content_type="text/markdown",
-  url="https://github.com/pb1729/factor-loss/empirical-init",
+  url="https://github.com/pb1729/empirical-init",
   author="Phillip Bement",
   author_email="{author_first_name}{author_last_initial}@fastmail.com",
   license="BSD 3-clause",
